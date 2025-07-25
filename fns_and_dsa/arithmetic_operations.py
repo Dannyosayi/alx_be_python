4f8bfad27ae38248f772f9aa4f78e43ed12d255f
Danny Oyobagie <oyobagiedanny@gmail.com>
2025-07-25 22:25:18 +0100

    add code

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1 ,num2 ,operation):    
+def perform_operation(num1, num2, operation):    
     match operation:
         case "subtract":
             result = num1 - num2
@@ -8,7 +8,7 @@ def perform_operation(num1 ,num2 ,operation):
             return result
         case "divide":
             if num2 == 0:
-                return result
+                return "Error: Cannot divide by zero"
             else: 
                 result = num1 / num2
             return result
